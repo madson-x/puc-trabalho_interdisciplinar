db17346188d2bd514184fa47294cc795d9a42279
eliafaustino <55817458+eliafaustino@users.noreply.github.com>
2019-11-26 19:24:08 -0300

    Add files via upload

--- reservas.php
+++ reservas.php
@@ -262,7 +262,7 @@ echo'</b> <p>';
 				  <div class="modal-dialog" role="document">
 					<div class="modal-content">
 					  <div class="modal-header">
-						<h5 class="modal-title" id="confirmaalteracao">Alteração realizada com sucesso.</h5>
+						<h5 class="modal-title" id="confirmaalteracao">Alteração da reserva realizada com sucesso.</h5>
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 						  <span aria-hidden="true">&times;</span>
 						</button>
