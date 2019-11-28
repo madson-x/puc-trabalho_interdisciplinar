4b20de3c9309fd37311529b2d40c3414d573e548
Robson Piere <robsonpiere@outlook.com>
2019-11-27 23:15:28 -0300

    melhoria layout

--- javascript/reservas.js
+++ javascript/reservas.js
@@ -20,9 +20,17 @@ function htmlReserva(item) {
     return cabecalhoReserva(item) + centroReserva(item);
 }
 
+function cancelar(id) {
+    confirm('cancelar');
+}
+
+function alterar(id) {
+    alert('alterar');
+}
+
 function cabecalhoReserva(item) {
     return [
-        '<div class="col-lg-4 col-12 my-3" style="float:left"></div>',
+        '<div class="col-lg-4 col-12 my-3" style="float:left">',
         '<p> Veja detalhes da sua reserva no <b>Quarto ' + item.acomodacao_descricao,
         '</b><br><br> Data de checkin: <b>' + item.data_entrada,
         '</b><br> Data de checkout: <b>' + item.data_saida,
@@ -35,7 +43,44 @@ function cabecalhoReserva(item) {
 function centroReserva(item)
 {
     return [
-
+        '<div class="col-lg-8 col-12 my-3" style="float:left">',
+            '<div class="card mb-3" style="max-width: 100%;">',
+                '<div class="row no-gutters">',
+                    '<div class="col-md-4">',
+                        '<div id="carouselLuxo" class="col-12 carousel slide" data-ride="carousel" style="padding:0px; margin:0px">',
+                            '<div class="carousel-inner">',
+                                '<div class="carousel-item active">',
+                                    '<img src="img/04.jpg" class="d-block w-100" alt="...">',
+                                '</div>',
+                                '<div class="carousel-item">',
+                                    '<img src="img/06.jpg" class="d-block w-100" alt="...">',
+                                '</div>',
+                                '<div class="carousel-item">',
+                                    '<img src="img/08.jpg" class="d-block w-100" alt="...">',
+                                '</div>',
+                            '</div>',
+                            '<a class="carousel-control-prev" href="#carouselLuxo" role="button" data-slide="prev">',
+                                '<span class="carousel-control-prev-icon" aria-hidden="true"></span>',
+                                '<span class="sr-only">Anterior</span>',
+                            '</a> ',
+                            '<a class="carousel-control-next" href="#carouselLuxo" role="button" data-slide="next">',
+                                '<span class="carousel-control-next-icon" aria-hidden="true"></span>',
+                                '<span class="sr-only">Próximo</span>',
+                            '</a>',
+                        '</div>',
+                    '</div>',
+                    '<div class="col-md-8">',
+                        '<div class="card-body">',
+							'<h5 class="card-title">Confira os detalhes do Quarto Luxo</h5>',
+								'<p class="card-text"><small class="text-muted">2 Pessoas - Wifi - TV 32" - Frigobar - Cama Casal King - Calefação - Ar condicionado - Hidro</small></p>',
+                                '<button onclick="cancelar('+ item.idreserva +')" type="button" class="btn btn-secondary">Cancelar Reserva</button>',
+                                '&nbsp;',
+                                '<button onclick="alterar(' + item.idreserva +')" type="button" class="btn btn-primary">Alterar Reserva</button>',
+				        '</div>',
+				    '</div>',        
+				'</div>',
+			'</div>',
+        '</div>'
     ].join('');
 }
 
